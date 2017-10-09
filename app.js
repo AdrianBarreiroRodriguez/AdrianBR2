cbd0ec4dc74df3d83d78ced47b900f5bd4b8317e
Adrian Barreiro Rodriguez <adrianmc2br86@gmail.com>
2017-10-09 12:53:24 +0200

    Subiendo codigo de la clase9/10/2017

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+
+//Crear conexión con m
+var dbURI = 'mongodb://localhost/';
+mongoose.connect(dbURI);
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
 
 //var sample = require('./routes/sample');
 
@@ -26,18 +41,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Front End
 app.all("*", (req, res) => {
-res.sendFile(path.resolve("public/index.html"));
+  res.sendFile(path.resolve("public/index.html"));
 })
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
