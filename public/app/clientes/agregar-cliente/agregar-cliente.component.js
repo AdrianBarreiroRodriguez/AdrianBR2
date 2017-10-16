fb8826146a06228be322c0f905b235694a3c6b84
Adrian Barreiro Rodriguez <adrianmc2br86@gmail.com>
2017-10-16 11:26:57 +0200

    Eliminar archivos de public

--- public/app/clientes/agregar-cliente/agregar-cliente.component.js
+++
@@ -1,6 +0,0 @@
-moduleClientes.component('agregarCliente', {
-    templateUrl: 'app/clientes/agregar-cliente/agregar-cliente.template.html',
-    controller: function (){
-        
-    }
-});
--- public/app/clientes/detalle-cliente/detalle-cliente.component.js
+++
@@ -1,6 +0,0 @@
-moduleClientes.component('detalleCliente', {
-    templateUrl: 'app/clientes/detalle-cliente/detalle-cliente.template.html',
-    controller: function (){
-        
-    }
-});
