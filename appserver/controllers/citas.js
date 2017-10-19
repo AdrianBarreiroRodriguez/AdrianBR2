f9e64e23a81eddd6aaf5b2d5ae977f9b1ca31e08
Adrian Barreiro Rodriguez <adrianmc2br86@gmail.com>
2017-10-19 23:02:01 +0200

    Implementacion a medio hacer del servicio citas:fromDate/toDate
    El mapa de citas se genera mediante bucles creando una cadena en formato
    json que represente el mapa, luego pasaremos la cadena a la funcion JSON.parse()
    que devolvera el mapa de citas como un objeto JSON.
    En caso de no conseguir crear el mapa de citas mediante esta estrategia
    utilizar el API de mongoose o la función reduce de JavaScript. Echar un
    vistazo a los repositorios de Santiago y Oscar para estas
    implementaciones alternativas.

--- appserver/controllers/citas.js
+++ appserver/controllers/citas.js
@@ -7,14 +7,59 @@ module.exports.calendarioDeCitas= function(fechaInicio, fechaFin){
     Cita.find({'fechaInicio':{$gt:fechaInicio},'fechaInicio':{$lte:fechaFin}}, function(err, citas){
         if(!err){
             console.log(citas);
-            crearObjetoJSONCalendario(citas);
+            return crearObjetoJSONCalendario(citas);
         }else{
             console.log(err);
         }
     });
 }
 
-function crearObjetoJSONCalendario(citas){
-    
+function crearObjetoJSONCalendario(citas, fechaInicio, fechaFin){
+    const horaInicio = "9:00";
+    const horaFin = "18:00";
+    var numeroDeHoras = numeroDeCitasDisponibles(horaInicio, horaFin);
+    var cadenaJSON = "{";
+    var numeroDeFechas= numeroDeDiasEntreFechas(fechaInicio, fechaFin);
+    var fechaBucle = fechaInicio;
+    for(var i=0; i<numeroDeFechas; i++){
+        cadenaJSON = cadenaJSON + fechaBucle + ":{";
+        var horaBucle = horaInicio;
+        for(var j=0; j<numeroDeHoras; j++){
+            cadenaJSON = cadenaJSON + horaBucle +":{";
+            var citaParaHoraActual = buscarCitaPorFechaYHora(fechaBucle, horaBucle);
+            var cadenaDatosCita = construirCadenaParaCita(citaParaHoraActual);
+            cadenaJSON = cadenaJSON + cadenaDatosCita + "}";
+            horaBucle = plusHora(horaBucle);
+        }
+        cadenaJSON = cadenaJSON + "}";
+        fechaBucle = plusDia(fechaBucle);
+    }
+    cadenaJSON = cadenaJSON + "}";
+    var objetoJSONCalendario = JSON.parse(cadenaJSON);
+    return objetoJSONCalendario;
 }
 
+
+function numeroDiasEntreFechas(fechaInicio, fechaFin){
+
+}
+
+function numeroDeCitasDisponibles(){
+
+}
+
+function buscarCitaPorFechaYHora(){
+
+}
+
+function construirCadenaParaCita(){
+
+}
+
+function plusDia(){
+
+}
+
+function plusHora(){
+    
+}
