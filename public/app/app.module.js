39c9fbbf33322570638e26db81876abadcb2b48f
Adrian Barreiro Rodriguez <adrianmc2br86@gmail.com>
2017-10-26 10:03:06 +0200

    Agrego directiva detalleCita-directives.js
    Falta agregar funcionalidad a la directiva para que al pasar por la
    cita, se muestre una vista expanadida superpuesta de los detalles de la cita.

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -15,4 +15,5 @@ angular.module('petStore',[
     'horarioCitasModule',
     'insertarCitaModule',
     'clienteRESTService',
+    'detalleCitaDirectivesModule',
 ]);
---
+++ public/app/directives/detalleCita-directives.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('detalleCitaDirectivesModule', [])
+        .directive('cita',function () {
+            var linker = function (scope, element, attrs) {
+                
+            };
+            var controller = function ($scope) {
+                
+            };
+            return {
+                restrict: 'A',
+                controller: controller,
+                controllerAs: 'cita',
+                link: linker,
+                template: '<span>{{hora}}</span>\
+                           <span>{{citas[hora].idMascota.nombre}}</span>\
+                           <span>{{citas[hora].idMascota.especie}}</span>\
+                           <span>{{citas[hora].estado}}</span>\
+                           <input type="button" value="detalle" ng-if="citas[hora]" ng-click="detalleCita(citas[hora]._id)">\
+                           <input type="button" value="insertar" ng-if="!citas[hora]" ng-click="irCrearCita(hora)">'
+            };
+});
